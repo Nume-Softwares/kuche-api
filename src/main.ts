92599d3d0628c3bfe1f27187417444ac7ccf1d5a
Ewerton Igor <ewertonigor@icloud.com>
2025-01-23 22:34:41 -0300

    feat: add swager and enviroment to main server

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { ConfigService } from '@nestjs/config'
+import { Env } from './env'
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {})
+
+  const configService = app.get<ConfigService<Env, true>>(ConfigService)
+
+  const port = configService.get('PORT', { infer: true })
+
+  const config = new DocumentBuilder()
+    .setTitle('Kuche API')
+    .setDescription('API para gerenciamento do software')
+    .setVersion('1.0')
+    .build()
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, documentFactory)
+
+  await app.listen(port)
+}
+
+bootstrap()
