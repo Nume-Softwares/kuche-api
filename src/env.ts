dbefb6d828394cd0a0187df906d2867353fba553
Ewerton Igor <ewertonigor@icloud.com>
2025-01-25 21:19:27 -0300

    feat: new enviroments

--- src/env.ts
+++ src/env.ts
@@ -4,6 +4,9 @@ export const envSchema = z.object({
   DATABASE_URL: z.string().url(),
   DIRECT_URL: z.string().url(),
   PORT: z.coerce.number().optional().default(3333),
+  JWT_SECRET_RESTAURANT: z.string(),
+  JWT_PRIVATE_KEY_RESTAURANT: z.string(),
+  JWT_PUBLIC_KEY_RESTAURANT: z.string(),
 })
 
 export type Env = z.infer<typeof envSchema>
