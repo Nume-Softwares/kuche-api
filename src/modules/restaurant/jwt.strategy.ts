15d38639b61c9db3cd22228d690c5f2e3f9313c5
Ewerton Igor <ewertonigor@icloud.com>
2025-01-25 21:20:52 -0300

    feat: config auth guard jwt

---
+++ src/modules/restaurant/jwt.strategy.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { PassportStrategy } from '@nestjs/passport'
+import { ExtractJwt, Strategy } from 'passport-jwt'
+import { Env } from '@/env'
+import { z } from 'zod'
+
+const tokenPayloadRestaurantSchema = z.object({
+  sub: z.string().uuid(),
+})
+
+export type TokenPayloadRestaurantSchema = z.infer<
+  typeof tokenPayloadRestaurantSchema
+>
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(config: ConfigService<Env, true>) {
+    const publicKey = config.get('JWT_PUBLIC_KEY_RESTAURANT', { infer: true })
+
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: Buffer.from(publicKey, 'base64'),
+      algorithms: ['RS256'],
+    })
+  }
+
+  async validate(payload: TokenPayloadRestaurantSchema) {
+    return tokenPayloadRestaurantSchema.parse(payload)
+  }
+}
