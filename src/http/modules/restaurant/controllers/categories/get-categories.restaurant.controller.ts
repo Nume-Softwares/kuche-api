60410d43aff58131a99662ccaf80a1f3e6893e29
Ewerton Igor <ewertonigor@icloud.com>
2025-01-28 17:36:26 -0300

    feat: get categories restaurant controller

---
+++ src/http/modules/restaurant/controllers/categories/get-categories.restaurant.controller.ts
@@ -0,0 +1,77 @@
+import { Controller, Get, HttpCode, Query } from '@nestjs/common'
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger'
+import { PrismaService } from '@/prisma/prisma.service'
+import { ZodValidationPipe } from '@/http/shared/pipes/zod-valitation-pipe'
+import { z } from 'zod'
+import { CurrentRestaurant } from '@/http/modules/current-restaurant.decorator'
+import { TokenPayloadRestaurantSchema } from '../../auth/jwt.strategy'
+
+const pageQueryParamSchema = z
+  .string()
+  .optional()
+  .default('1')
+  .transform(Number)
+  .pipe(z.number().min(1))
+
+type PageQueryParamSchema = z.infer<typeof pageQueryParamSchema>
+
+const queryValidationPipe = new ZodValidationPipe(pageQueryParamSchema)
+
+@ApiTags('Categorias')
+@ApiBearerAuth('access-token')
+@Controller('/restaurant/categories')
+export class GetCategoriesRestaurantController {
+  constructor(private prisma: PrismaService) {}
+
+  @Get()
+  @HttpCode(200)
+  @ApiOperation({ summary: 'Lista todas as categorias' })
+  @ApiResponse({
+    status: 200,
+    description: 'Retorna todas as categorias',
+  })
+  @ApiResponse({ status: 401, description: 'Não Autorizado' })
+  async handle(
+    @CurrentRestaurant() payload: TokenPayloadRestaurantSchema,
+    @Query('page', queryValidationPipe) page: PageQueryParamSchema,
+  ) {
+    const perPage = 8
+
+    const totalCategories = await this.prisma.category.count({
+      where: {
+        restaurantId: payload.restaurantId,
+      },
+    })
+
+    if (totalCategories === 0) {
+      return { members: [], totalPages: 0 }
+    }
+
+    const totalPages = Math.ceil(totalCategories / perPage)
+
+    const getCategories = await this.prisma.category.findMany({
+      take: perPage,
+      skip: (page - 1) * perPage,
+      where: {
+        restaurantId: payload.restaurantId,
+      },
+      select: {
+        id: true,
+        name: true,
+        isActive: true,
+        _count: {
+          select: {
+            menuItems: true,
+          },
+        },
+      },
+    })
+
+    return { members: getCategories, totalPages }
+  }
+}
