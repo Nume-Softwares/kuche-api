e9f204a992df73c7e0b14ea85a660f512dab7c5a
Ewerton Igor <ewertonigor@icloud.com>
2025-02-08 12:55:24 -0300

    fix: env optional

--- src/env.ts
+++ src/env.ts
@@ -2,7 +2,7 @@ import { z } from 'zod'
 
 export const envSchema = z.object({
   DATABASE_URL: z.string().url(),
-  DIRECT_URL: z.string().url(),
+  DIRECT_URL: z.string().url().optional(),
   PORT: z.coerce.number().optional().default(3333),
   PORT_DEV: z.coerce.number().optional().default(3000),
   JWT_SECRET_RESTAURANT: z.string(),
